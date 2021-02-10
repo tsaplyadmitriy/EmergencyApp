cdd831df7804d1a3234c7004b7ed3f0fc8a9e83d
WinnerOK <danman339@gmail.com>
2021-02-10 23:28:38 +0300

    Reformat code

--- emergency_app/lib/app.dart
+++ emergency_app/lib/app.dart
@@ -1,30 +1,20 @@
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'card_ui/card_ui_page.dart';
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
       title: 'Flutter Demo',
-
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
-
         visualDensity: VisualDensity.adaptivePlatformDensity,
         unselectedWidgetColor: Color(0xFFEB5757),
         toggleableActiveColor: Color(0xFFEB5757),
       ),
-
       home: CardUIPage(),
-
-
     );
   }
 }
