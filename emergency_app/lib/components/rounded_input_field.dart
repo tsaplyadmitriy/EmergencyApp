6e837cdf4b1d55d27f559b552bdfbba8850b6cb3
wiirtex <50456151+wiirtex@users.noreply.github.com>
2021-02-10 23:13:21 +0500

    Create rounded_input_field.dart

---
+++ emergency_app/lib/components/rounded_input_field.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class RoundedInputField extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _RoundedInputFieldState();
+
+
+
+}
+
+class _RoundedInputFieldState extends State<RoundedInputField> {
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+        decoration: beautifulDecoration("flex")
+    );
+  }
+}
+
+
+InputDecoration beautifulDecoration(String inputText) {
+  return new InputDecoration(
+      hintText: inputText,
+
+      filled: true,
+      fillColor: Color(0xFFF2F2F2),
+      focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: BorderRadius.circular(12.0)
+      ),
+      enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: BorderRadius.circular(12.0)
+      )
+  );
+
+
+}
