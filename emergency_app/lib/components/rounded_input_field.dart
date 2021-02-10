bc7b0a38a7d08d5bc4209b7ea80b794f7355e385
wiirtex <otim142@ya.ru>
2021-02-10 20:19:13 +0300

    Try again

--- emergency_app/lib/components/rounded_input_field.dart
+++ emergency_app/lib/components/rounded_input_field.dart
@@ -12,27 +12,27 @@ class _RoundedInputFieldState extends State<RoundedInputField> {
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-      decoration: beautifulDecoration("flex")
+        decoration: beautifulDecoration("flex")
     );
   }
 }
 
 
 InputDecoration beautifulDecoration(String inputText) {
-    return new InputDecoration(
-        hintText: inputText,
+  return new InputDecoration(
+      hintText: inputText,
 
-        filled: true,
-        fillColor: Color(0xFFF2F2F2),
-        focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide.none,
-            borderRadius: BorderRadius.circular(12.0)
-        ),
-        enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide.none,
-            borderRadius: BorderRadius.circular(12.0)
-        )
-    );
+      filled: true,
+      fillColor: Color(0xFFF2F2F2),
+      focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: BorderRadius.circular(12.0)
+      ),
+      enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: BorderRadius.circular(12.0)
+      )
+  );
 
 
 }
